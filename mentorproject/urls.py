39ad87cdbd8609c9b7e274420b2ea997d3baaed0
Danielbjorn <danielbjorn94@gmail.com>
2022-06-29 18:11:21 +0000

    Initial Commit

---
+++ mentorproject/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+urlpatterns = [
+
+		path('', include('mentorapp.urls')),
+		path('admin/', admin.site.urls),
+ 
+] 
